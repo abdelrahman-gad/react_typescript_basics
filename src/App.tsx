80021db59ee6b5a0b6bcee75d38a0043401e8cf8
abdelrahmangad95@gmail.com <abdelrahmangad95@gmail.com>
2022-06-08 14:14:01 +0200

    restricting props

--- src/App.tsx
+++ src/App.tsx
@@ -19,42 +19,14 @@ import './App.css';
 // import { DomRef } from './components/DomRef';
 // import { Private } from './components/auth/Private';
 // import { Profile } from './components/auth/Profile';
-import { List } from './components/generics/List';
+// import { List } from './components/generics/List';
+
+import {RandomNumber} from './components/restriction/RandomNumber'; 
+
 function App() {
   return (
     <div className="App">
-      <List
-        items={['item1', 'item2', 'item3']}
-        onClick={(item) => console.log(item)}
-
-      />
-      <List
-        items={[1, 4, 5]}
-        onClick={(item) => console.log(item)}
-
-      />
-      <List
-        items={[
-          {
-            id:1,
-            first: 'Oscar',
-            last: 'Gonzalez'
-          },
-          {
-            id:2,
-            first: 'abdo',
-            last: 'gad'
-          },
-          {
-            id:3,
-            first: 'abdelrahman',
-            last: 'azmy'
-          }
-        ]}
-
-        onClick={(item) => console.log(item)}
-
-      />
+      <RandomNumber value={0}  isZero />
     </div>
   );
 }
