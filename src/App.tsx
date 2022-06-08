d556a88d2a4e6a44d793410480c7557574e7e330
abdelrahmangad95@gmail.com <abdelrahmangad95@gmail.com>
2022-06-08 14:00:34 +0200

    generics

--- src/App.tsx
+++ src/App.tsx
@@ -17,14 +17,44 @@ import './App.css';
 // import {UserContextProvider} from './components/context/UserContext';
 // import {User} from './components/context/User';
 // import { DomRef } from './components/DomRef';
-import { Private } from './components/auth/Private';
-import { Profile } from './components/auth/Profile';
+// import { Private } from './components/auth/Private';
+// import { Profile } from './components/auth/Profile';
+import { List } from './components/generics/List';
 function App() {
-
-
   return (
     <div className="App">
-      <Private isLoggedIn={true} component={Profile} />
+      <List
+        items={['item1', 'item2', 'item3']}
+        onClick={(item) => console.log(item)}
+
+      />
+      <List
+        items={[1, 4, 5]}
+        onClick={(item) => console.log(item)}
+
+      />
+      <List
+        items={[
+          {
+            id:1,
+            first: 'Oscar',
+            last: 'Gonzalez'
+          },
+          {
+            id:2,
+            first: 'abdo',
+            last: 'gad'
+          },
+          {
+            id:3,
+            first: 'abdelrahman',
+            last: 'azmy'
+          }
+        ]}
+
+        onClick={(item) => console.log(item)}
+
+      />
     </div>
   );
 }
---
+++ src/components/generics/List.tsx
@@ -0,0 +1,21 @@
+type ListProps <T> ={
+    items:T[] ,
+    onClick:(value:T)=>void
+}
+
+export const List = <T extends {}>({items,onClick}:ListProps<T>)=>{
+     return (
+        <div>
+            <h1>List of Items</h1>
+            {
+                items.map((item,index)=>{ 
+                 return (
+                     <div key={index} onClick={()=>onClick(item)}>
+                        { item}
+                     </div>
+                 )
+                })
+            }
+        </div>
+    );
+}
